0169b5dbfbe159d72a4385d414ab799f40692f01
RareReaper <72725806+RareReaper@users.noreply.github.com>
2024-07-16 16:59:24 +0000

    FS #3

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask, redirect, render_template, request, session
 from helper import login_required
+from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tasks.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(debug=True)
+
